67d6a1c052f28c3e882a4cea0327303bfc8b9ad9
Ryan Howard <ryanghoward@protonmail.com>
2022-08-08 14:13:32 -0700

    Nav and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Bonjour</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
