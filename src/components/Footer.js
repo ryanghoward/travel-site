46e15dd18222529adb816f19a7840337ce839cb1
Ryan Howard <ryanghoward@protonmail.com>
2022-08-09 10:14:58 -0700

    react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "../CSS/Footer.css";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
 
 function Footer() {
   return (
@@ -44,14 +51,14 @@ function Footer() {
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h2>Videos</h2>
             <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
             <Link to='/'>Agency</Link>
             <Link to='/'>Influencer</Link>
           </div>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
@@ -68,48 +75,13 @@ function Footer() {
             </Link>
           </div>
           <small className='website-rights'>TRVL © 2022</small>
-          {/* <div className='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
-          </div> */}
+          <div className='social-icons'>
+            <FaFacebook className='social-icon-link' />
+            <FaInstagram className='social-icon-link' />
+            <FaYoutube className='social-icon-link' />
+            <FaTwitter className='social-icon-link' />
+            <FaLinkedin className='social-icon-link' />
+          </div>
         </div>
       </section>
     </div>
